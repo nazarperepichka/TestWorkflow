a397627370f9a5037961ad3d88c6ebcd262f88b2
Nazar Perepichka <nazar.perepichka@gmail.com>
2024-09-02 08:24:21 +0300

    initial commit

---
+++ App1.Console/SomeLogic.cs
@@ -0,0 +1,10 @@
+﻿namespace App1.Console
+{
+    public static class SomeLogic
+    {
+        public static bool GetValue(bool value)
+        {
+            return value;
+        }
+    }
+}
---
+++ App1.Tests/SomeLogicTests.cs
@@ -0,0 +1,17 @@
+﻿using App1.Console;
+using Xunit;
+
+namespace App1.Tests
+{
+    public class SomeLogicTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ShouldReturnValue(bool value)
+        {
+            var res = SomeLogic.GetValue(value);
+            Assert.Equal(value, res);
+        }
+    }
+}
